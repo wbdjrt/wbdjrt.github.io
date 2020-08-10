e574ca85dc12fd04b14b3bf42498aaa5412c7011
wbdjrt <66834264+wbdjrt@users.noreply.github.com>
2020-08-10 15:42:37 +0300

    Initial Commit

---
+++ js/donations.js
@@ -0,0 +1,326 @@
+export const tagslist = [
+    
+    {
+        id:1,
+        name:"Money"
+    },
+    {
+        id:9,
+        name:"Food"
+    }
+    ,
+    {
+        id:2,
+        name:"Clothes"
+    }
+]
+
+export const list = [
+
+    {
+        id: 0,
+        name: "Lebanese Red Cross",
+        excerpt: "The Lebanese Red Cross (LRC) is a national society that was established on July 9, 1945 as an independent National Society. In 1946, it was recognized by the State as a public nonprofit organization and as an auxiliary team to the medical personnel of the Lebanese Army...",
+        activeurl: "https://www.supportlrc.app/",
+        weburl: "http://www.redcross.org.lb/index.aspx?pageid=907",
+        imgsrc: "assets/imgs/donations/lrc.gif",
+        tags: [1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    }
+    ,
+    {
+        id: 1,
+        name: "Donner Sans Compter",
+        excerpt: "DSC is a non-governmental, non-profit organization founded onthe principles of empathy,compassion, humility, and sacrifice. We are not politically aligned. We are notreligiously affiliated. Ouronly commitment is to humanity. DSC links patients in need of blood donationsand those who are willing todonate...",
+        activeurl: "https://www.givingloop.org/dsc",
+        weburl: "https://dsclebanon.org/",
+        imgsrc: "assets/imgs/donations/dsc.jpg",
+        tags: [1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    }
+    ,
+    {
+        id: 2,
+        name: "arcenciel",
+        excerpt: `Adhering to the principles of sustainable development, 
+        arcenciel emphasizes the social and economic integration of marginalized people and communities back into society. 
+        It believes that every person, no matter the gravity of their handicap, 
+        is capable of overcoming their disability and contributing to the communities in which they live. 
+        arcenciel supports the diversity of society, 
+        and encourages the sustainability and conservation of natural resources in all aspects of its work...`,
+        activeurl: "https://www.arcenciel.org/donate/",
+        weburl: "https://www.arcenciel.org/en/",
+        imgsrc: "assets/imgs/donations/aec.jpg",
+        tags: [1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    },
+    {
+        id: 3,
+        name: "Impact Lebanon",
+        excerpt: `Impact Lebanon is a non-profit organisation that brings the community together to pursue initiatives that deliver impact for Lebanon. 
+        Our process is to encourage the brainstorming of ideas; 
+        promote the sharing of knowledge, resources and expertise; 
+        and build strong teams to turn an idea into a successful initiative...`,
+        activeurl: "https://www.justgiving.com/crowdfunding/lebanon-relief?utm_term=PYp7gXzyD",
+        weburl: "https://www.impactlebanon.org/",
+        imgsrc: "assets/imgs/donations/impactlebanon.jpg",
+        tags: [1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    },
+    {
+        id: 4,
+        name: "Saint George Hospital",
+        excerpt: `Saint George Hospital University Medical Center is a nonprofit academic medical center, 
+        owned by the Orthodox Archdiocese of Beirut and is academically affiliated to the University of Balamand. 
+        It is committed to treat all patients with dignity, respect and compassion regardless of their religion, nationality, 
+        socioeconomic status and race. 
+        It provides quality healthcare services to the sick and indigent at the lowest possible cost...`,
+        activeurl: "https://www.stgeorgehospital.org/stgeorge-donation",
+        weburl: "https://www.stgeorgehospital.org/",
+        imgsrc: "assets/imgs/donations/stgeorgehospital.png",
+        tags: [1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    },
+    {
+        id: 5,
+        name: "Lebanon of Tomorrow",
+        excerpt: `Lebanon Of Tomorrow is a Lebanese NGO that launched in 2020, 
+        with the objective to provide vulnerable categories of the Lebanese people with social assistance, 
+        particularly in times of economic and financial crises...`,
+        activeurl: "http://lebanonoftomorrow.org/product/donate-for-beirut/",
+        weburl: "https://lebanonoftomorrow.org/",
+        imgsrc: "assets/imgs/donations/LOT.png",
+        tags: [1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    },
+    {
+        id: 6,
+        name: "Sesobel",
+        excerpt: `Since 1976, SESOBEL has taken all possible steps to implement a coherent and monitored service of assisting children with disability and to accompany their families as partners in facing life’s challenges.
+        SESOBEL has worked equally hard with all elements of the society to recognize the value, dignity, and respect for children with disability...`,
+        activeurl: "https://www.sesobel.org/en/sengager/faireundon",
+        weburl: "https://www.sesobel.org/",
+        imgsrc: "assets/imgs/donations/sesobel.png",
+        tags: [1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    }
+    ,
+    {
+        id: 7,
+        name: "Lebanese Food Bank",
+        excerpt: ``,
+        activeurl: "https://donate.lebanesefoodbank.org/",
+        weburl: "https://lebanesefoodbank.org/",
+        imgsrc: "assets/imgs/donations/lfb.png",
+        tags: [9,1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    },
+    {
+        id: 8,
+        name: "Caritas",
+        excerpt: `Caritas Lebanon, the common official socio-pastoral arm of the local Catholic Church in service of the poor and the promotion of love, 
+        charity and justice, provides economic development, livelihoods, health and social care, education, migration services, 
+        emergency and crisis intervention, human and humanitarian relief and aid, environmental stewardship, 
+        as well as advocacy and protection for all individuals and groups of people in need...`,
+        activeurl: "http://www.caritas.org.lb/get_involved/donate",
+        weburl: "http://www.caritas.org.lb/",
+        imgsrc: "assets/imgs/donations/caritas.png",
+        tags: [1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    },
+    {
+        id: 9,
+        name: "Offrejoie",
+        excerpt: `Offrejoie was born in the midst of a raging civil war in Lebanon in 1985. 
+        A group of young Red Cross volunteers worked together to help the injured. 
+        Alleviating human suffering transcended any differences between them. They committed to heal their torn country. 
+        They started with children’s camps during the conflict...`,
+        activeurl: "https://offrejoie.org/donate/",
+        weburl: "https://offrejoie.org/",
+        imgsrc: "assets/imgs/donations/offrejoie.png",
+        tags: [1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    },
+    {
+        id: 10,
+        name: "Live Love Rebuild Beirut",
+        excerpt: `We are an independent non-profit founded in 2012 in Beirut and Los Angeles with the goal of sharing, 
+        celebrating and helping the beauty of Lebanon...`,
+        activeurl: "https://livelove.org/donate",
+        weburl: "https://livelove.org/rebuild_beirut",
+        imgsrc: "assets/imgs/donations/liveloverebuildbeirut.png",
+        tags: [1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    },
+    {
+        id: 11,
+        name: "Baytna Baytak",
+        excerpt: `In the wake of the tragic Beirut blast, 
+        Baytna Baytak has shifted its priority to relocating displaced individuals who have lost their homes to the blast. 
+        More than 300,000 Lebanese citizens need our assistance. 
+        Please help us help them...`,
+        activeurl: "https://www.justgiving.com/crowdfunding/impact-lebanon?utm_term=dXEJkepP7",
+        weburl: "https://www.baytnabaytak.com/home",
+        imgsrc: "assets/imgs/donations/baytnabaytak.png",
+        tags: [1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    },
+    {
+        id: 12,
+        name: "Beit El Baraka",
+        excerpt: `Beit el Baraka is a non-profit organization established in Beirut in 2018, and recognized by the Lebanese state by virtue of Decree No 2277 ...`,
+        activeurl: "https://beitelbaraka.net/donation/pay/",
+        weburl: "https://www.beitelbaraka.org/",
+        imgsrc: "https://www.beitelbaraka.org/wp-content/uploads/2020/06/1-04.png",
+        tags: [9, 1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    },
+    {
+        id: 13,
+        name: "Act For Lebanon USA",
+        excerpt: `Act for Lebanon-USA (ACT) is a 501(c)(3) not-for-profit organization established in the US with a mission to support various causes in Lebanon including children, education, health and mental health, human rights, palliative care, women empowerment, environment, as well as cultural and athletic programs...`,
+        activeurl: "https://www.actforlebanonusa.org/",
+        weburl: "https://www.actforlebanonusa.org/",
+        imgsrc: "https://www.actforlebanonusa.org/wp-content/uploads/2019/12/logo.png",
+        tags: [1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    }
+    ,
+    {
+        id: 14,
+        name: "Lebanon Needs",
+        excerpt: `Lebanon Needs is a non-profit organization founded in 2019 dedicated to creating and delivering sustainable healthcare solutions for impoverished and underserved Lebanese...`,
+        activeurl: "https://www.lebanonneeds.org/donate",
+        weburl: "https://www.lebanonneeds.org/what",
+        imgsrc: "assets/imgs/donations/ly.png",
+        tags: [1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    },
+    {
+        id: 15,
+        name: "nusaned",
+        excerpt: `Nusaned is a humanitarian, community-based and volunteer organization.  We believe that the power to create change comes from working collaboratively.
+
+        Nusaned does not engage in any political activity, and remains at equal distance from all religions...`,
+        activeurl: "https://www.nusaned.org/en/donate",
+        weburl: "https://www.nusaned.org/en",
+        imgsrc: "https://www.nusaned.org/assets/img/logo.png",
+        tags: [1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    },
+    {
+        id: 16,
+        name: "Chance Association",
+        excerpt: `    (CHildren AgaiNst CancEr) is a Lebanese Association whose mission is to help Lebanese children with Cancer or Blood Disorders receive the best chance for cure and acceptation of their disease...`,
+        activeurl: "https://chanceassociation.org/donate",
+        weburl: "https://chanceassociation.org/home",
+        imgsrc: "https://bmbc.cloud/storage/companies/logos/web/83544_2019_9_10.png",
+        tags: [1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    },
+    {
+        id: 17,
+        name: "bassma",
+        excerpt: `BASSMA believes that Healthy Families are the pillars of a Healthy Society.
+
+
+        Every person has the right to live in dignity and deserves a chance to achieve her potential through access to Food, Water, Clothing, Education, Employment, HealthCare, Housing, Safe and Secure Environment, and Decent Living Standards...`,
+        activeurl: "https://www.bassma.org/DonateNow/donateonline/",
+        weburl: "https://www.bassma.org/",
+        imgsrc: "https://bassma.org/Content/images/ilogo.png",
+        tags: [1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    }
+    ,
+    {
+        id: 18,
+        name: "Hopital Libanais Geitaoui",
+        excerpt: `The Lebanese Hospital Geitaoui - UMC, one of the first hospitals in Lebanon, was founded in 1927 by father Joseph Selwan Geitaoui. The hospital administration was entrusted by the founder to the Congregation of the Maronite Sisters of the Holy Family established by Patriarch Elias El Hoyek in 1895...`,
+        activeurl: "https://www.hopital-libanais.com/otherdefinitions/donate",
+        weburl: "https://www.hopital-libanais.com/",
+        imgsrc: "https://www.hopital-libanais.com/img/logo.png",
+        tags: [1],
+        instagram:"",
+        facebook:"",
+        twitter:"",
+        email:"",
+        number:""
+    }
+
+
+
+
+
+]
